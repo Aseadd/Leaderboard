97d534da450ed5c6a789eb345cb865b39b97c8a2
addis <betsega23@gmail.com>
2022-08-26 01:34:24 +0300

    Add stylesheet to the leaderboard

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import './styles.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import display from './module/display';
 
 const game = require('./module/game.js');
 const scoreResult = require('./module/scoreResult.js');
 // eslint-disable-next-line
 const url =
-  'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/xXcSDd9DQXixIyWfrbiy/scores';
-display();
+  'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/y87mZKQ441q4wBL6veJQ/scores';
+//display();
 scoreResult(url);
 
 const refresh = document.querySelector('.refresh-btn');
@@ -17,6 +18,8 @@ const inputScore = document.getElementById('score');
 submit.addEventListener('click', async () => {
   const user = inputName.value;
   const score = inputScore.value;
+  inputName.value = '';
+  inputScore.value = '';
   await game.setScores(url, user, score);
   await scoreResult(url);
 });
--- src/module/display.js
+++ src/module/display.js
@@ -3,18 +3,21 @@ export default function display() {
 
   const titleSection = document.createElement('div');
   titleSection.classList.add('title-section');
+  titleSection.classList.add('row');
 
   const outputTitle = document.createElement('h2');
   outputTitle.innerHTML = 'Recent Scores';
+  outputTitle.classList.add('col');
   titleSection.appendChild(outputTitle);
 
   const refresh = document.createElement('button');
   refresh.classList.add('refresh-btn');
+  refresh.classList.add('col-lg-2');
+  refresh.classList.add('btn');
+  refresh.classList.add('btn-primary');
+  refresh.classList.add('btn-sm');
   refresh.innerHTML = 'Refresh';
   titleSection.appendChild(refresh);
   result.appendChild(titleSection);
-  const score = document.createElement('div');
-  score.classList.add('scores');
-  result.appendChild(score);
   return result;
 }
